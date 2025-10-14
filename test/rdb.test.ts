189280fba0ef95bfa216053f42d9d569c76f9e3d
gilons <gf.694765457@gmail.com>
2025-10-14 07:25:55 +0100

    Initial commit

---
+++ test/rdb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Rdb from '../lib/rdb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rdb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Rdb.RdbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
